5dd61e6a7b0462cf7c8afb20730591bf35b88744
Sushanth2805 <sushanth.2805@gmail.com>
2026-01-21 22:12:29 +0530

    changed it - not a great looking one but decent

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,14 @@ export default defineConfig(({ mode }) => {
     return {
       server: {
         port: 3000,
-        host: '0.0.0.0',
+        host: 'localhost',
       },
       plugins: [react()],
+      root: '.',
+      build: {
+        outDir: 'dist',
+        sourcemap: true,
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
